795fe91918d37502697a2f8d6f97d9df1a57123d
Oleg Kiselev <e5ash.bro@gmail.com>
2022-04-27 10:14:05 +0300

    Init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script setup>
+  useHead({
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'utf-8',
+    meta: [
+      { name: 'description', content: '' }
+    ],
+  })
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>Lorem</p>
+</template>
+<script>
+  export default {
+    head: {
+      title: ''
+    },
+    setup() {
+      definePageMeta({
+        title: ''
+      });
+    }
+  }
+</script>
