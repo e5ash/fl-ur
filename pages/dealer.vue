8cbd735a90050576c57afa60554634c8c514bd7f
Oleg Kiselev <e5ash.bro@gmail.com>
2022-05-06 02:39:20 +0300

    Add pages

---
+++ pages/dealer.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="main">
+  </main>
+</template>
+<script>
+  export default {
+    head: {
+      title: ''
+    },
+    setup() {
+      definePageMeta({
+        title: ''
+      });
+    }
+  }
+</script>
+
+
---
+++ pages/selection.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="main">
+  </main>
+</template>
+<script>
+  export default {
+    head: {
+      title: ''
+    },
+    setup() {
+      definePageMeta({
+        title: ''
+      });
+    }
+  }
+</script>
+
